7bdb5100399120669c0cfa8f84f6abaaf6549b1d
Hugo Alliaume <kocal@live.Fr>
2019-07-23 17:19:09 +0200

    chore(deps): install twig bundle

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Twig\CssInliner\Bundle\TwigCssInlinerBundle::class => ['all' => true],
     Twig\Inky\Bundle\TwigInkyBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
