734d299c5d910b18b263bfe8863f33f5cb4c11a2
Hugo Alliaume <kocal@live.Fr>
2019-07-23 17:15:36 +0200

    chore(deps): install Inky and CSS Inliner Twig extension

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,6 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Twig\CssInliner\Bundle\TwigCssInlinerBundle::class => ['all' => true],
+    Twig\Inky\Bundle\TwigInkyBundle::class => ['all' => true],
 ];
